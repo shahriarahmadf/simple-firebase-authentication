818a66ca55244aea9a139987f541361298153e1c
shahriarahmadf <shahriarahmadf@gmail.com>
2022-11-06 16:55:20 +0900

    connected git login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
 
   const provider = new GoogleAuthProvider();
 
@@ -12,6 +14,7 @@ function App() {
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error => {
@@ -19,10 +22,39 @@ function App() {
     })
   }
 
+  const handleSignOut = () => {
+    signOut(auth)
+    .then( () => {
+      setUser({});
+    })
+    .catch( () => {
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
+      {/* condition ? true : false */}
 
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+      {
+        user.email ? 
+          <button onClick={handleSignOut}>Sign Out</button>
+        :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+            <button>GitHub Sign In</button>
+          </>
+      }
+
+      {
+        user.email && 
+          <div>
+            <h3>User name: {user.displayName}</h3>
+            <p>Email address: {user.email}</p>
+            <img src={user.photoURL} alt="" />
+          </div>
+      }
 
     </div>
   );
